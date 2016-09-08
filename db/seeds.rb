029570d80a0ac08dacb6cccc3172f4e67d09b7ff
ArsGilani <gilani.vu@gmail.com>
2016-09-08 20:34:40 +0600

    propisal seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+user_admin = User.create(name: 'Admin', email: 'admin@example.com', password: '123456', password_confirmation: '123456')
+
+user_user = User.create(name: 'Gilani', email: 'gilani@example.com', password: '123456', password_confirmation: '123456')
+
+post1 = Post.create(
+	description: 'Обедаю',  
+	image:  File.new("#{Rails.root}/public/uploads/obed.jpg"),
+	user_id: user_admin.id
+  
+)
+
+post2 = Post.create(
+  description: 'Alarcha',  
+  image:  File.new("#{Rails.root}/public/uploads/alarcha.jpg"),
+  user_id: user_user.id
+)
+
+Comment.create(
+		description: 'Обычные дни админа))', 
+		user_id: user_admin.id,
+		post_id: post1.id
+	)
+Comment.create(
+		description: 'Едил на веле в ущелье', 
+		user_id: user_user.id,
+		post_id: post2.id
+	)
